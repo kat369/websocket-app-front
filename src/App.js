17210fb0e86248f58f9648e0cd8d28a57fbbdf3b
kat369 <kathiravanarjunan369@gmail.com>
2023-12-08 23:49:19 +0530

    api old

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import "./App.css";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:5000/");
+const socket = io.connect("https://websocket-app-back-je1tv34v9-kat369.vercel.app/");
 
 function App() {
   const [userName, setUserName] = useState("");
